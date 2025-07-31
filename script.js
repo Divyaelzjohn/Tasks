7c5d6902eba5602f0579822e2983d94b38af9d1b
Divya <divelzjon@gmail.com>
2025-07-31 22:37:33 +0530

    primenumber

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-function isprime(n){
-  if(n=>0)
-    for(let i=2;i<=n-1;i++){
-      remainder(n,i);
-    }
-  }
+// function isprime(n){
+//   if(n=>0)
+//     for(let i=2;i<=n-1;i++){
+//       if(remainder(n,i)!=0){
+//         console.log("prime")
+//       }
+//       else{
+//         console.log("not a prime number")
+//       }
+//       // if(remainder(n!=0)){
+//       //   console.log('not prime')
+//       // }
+//       // if(remainder()===false){
+//       //   console.log('prime')
+//       // }else{
+//       //   console.log('not prime')
+//       // }
+//     }
+//   }
+// isprime(8)
 
-isprime(22)
-
-function remainder(n,i){
-  if(n-(Math.floor(n/i))*i+i-i===0){
-    console.log(n+' divisible by a number')
-    return false;
-  }
-  else{
-    console.log(n+' is a prime')
-  }
-}
+// function remainder(n,i){
+//   if(n-(Math.floor(n/i))*i+i-i===0){
+//     // console.log(' divisible')
+//     return true;
+//   }else{
+//     // console.log('not divisible')
+//     return false
+//   }
+// }
 
--- script1.js
+++ script1.js
@@ -5,3 +5,24 @@
 // console.log(Math.floor(c))
 // console.log(n-c)
 // console.log(22-(Math.floor(22/2))*2+2-2)
+
+
+
+function remainder(n){
+  let isPrime=true;
+  for(let i=2;i<=n-1;i++){
+
+    let result=n-((Math.floor(n/i)*i)+i-i)
+    if(result===0){
+      console.log("Not prime number")
+      isPrime=false;
+      break;
+    }
+  }
+  
+  if(isPrime){
+    console.log("prime number")
+  }
+}
+remainder(77)
+
